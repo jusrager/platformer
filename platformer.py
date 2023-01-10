6ad8384824ffb25d47be99a3aeb711e9e42ebf9b
Julia <Julia>
2023-01-10 19:20:04 +0300

    first

--- platformer.py
+++ platformer.py
@@ -2,15 +2,22 @@ import pygame
 import os
 import sys
 
-width = 800
-height = 600
+width = 832
+height = 384
+
+tile_width = tile_height = 64
 
 
 def load_image(fullname, colorkey=None):
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
+
     image = pygame.image.load(fullname)
+
+    if fullname != 'background.png':
+        image = pygame.transform.scale(image, (tile_width, tile_height))
+
     if colorkey is not None:
         image = image.convert()
         if colorkey == -1:
@@ -22,8 +29,8 @@ def load_image(fullname, colorkey=None):
 
 
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
-FPS = 10
+screen = pygame.display.set_mode((width, height))
+FPS = 40
 clock = pygame.time.Clock()
 x, y = 0, 0
 r = 10
@@ -41,45 +48,47 @@ player_image = load_image('player.png')
 background_image = load_image('background.png')
 
 
-tile_width = tile_height = 64
-
-
 class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group, all_sprites)
         self.image = tile_images[tile_type]
         self.rect = self.image.get_rect().move(
-            tile_width * pos_x, height - tile_height * (pos_y + 1))
+            tile_width * pos_x, tile_height * pos_y)
         self.image = pygame.transform.scale(
-            self.image, (self.image.get_width() // 2, self.image.get_height() // 2))
+            self.image, (tile_width, tile_height))
 
 
 class Player(pygame.sprite.Sprite):
+    STEP = 2
+
     def __init__(self, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
         self.image = player_image
 
+        print(pos_x, pos_y)
         self.rect = self.image.get_rect().move(
-            self.image.get_width() * pos_x, self.image.get_height() * pos_y)
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-
-        self.image = pygame.transform.scale(
-            self.image, (self.image.get_width() // 2, self.image.get_height() // 2))
+            tile_width * pos_x, tile_height * pos_y - 2)
 
         self.run = {'right': [os.path.join('walk', 'right', file) for file in os.listdir(os.path.join('walk', 'right'))],
                     'left': [os.path.join('walk', 'left', file) for file in os.listdir(os.path.join('walk', 'left'))]}
 
         print(self.run)
+
         self.cur_img = 0
         self.is_move = False
         self.direction = 'right'
         self.is_up = False
         self.up_time = 0
+        self.is_falling = False
 
     def update(self) -> None:
+        if not pygame.sprite.spritecollideany(self, tiles_group) and self.is_falling:
+            self.lending()
+
         # print(pygame.time.get_ticks())
+
         images = self.run[self.direction]
+
         if self.is_move:
             self.move()
             self.cur_img = (self.cur_img + 1) % len(images)
@@ -88,23 +97,34 @@ class Player(pygame.sprite.Sprite):
             self.image = load_image(images[0])
 
         if self.is_up:
-            if pygame.time.get_ticks() - self.up_time >= 500:
+            if pygame.time.get_ticks() - self.up_time >= 400:
                 print(pygame.time.get_ticks(), self.up_time)
                 self.is_up = False
 
-        self.image = pygame.transform.scale(
-            self.image, (self.image.get_width() // 2, self.image.get_height() // 2))
-
     def move(self):
-        print(self.pos_x, self.pos_y)
-        self.pos_x += 1
+        if self.direction == 'right':
+            self.rect = self.rect.move(self.STEP, 0)
+
+            if pygame.sprite.spritecollideany(self, tiles_group):
+                self.rect = self.rect.move(-self.STEP, 0)
+        elif self.direction == 'left':
+            self.rect = self.rect.move(-self.STEP, 0)
+
+            if pygame.sprite.spritecollideany(self, tiles_group):
+                self.rect = self.rect.move(self.STEP, 0)
+
         if self.is_up:
-            self.rect = self.image.get_rect().move(
-                self.image.get_width() * self.pos_x, self.image.get_height() * (self.pos_y + 1))
-        else:
-            self.rect = self.image.get_rect().move(
-                self.image.get_width() * self.pos_x, self.image.get_height() * (self.pos_y + 2))
-        # collide
+            self.rect = self.rect.move(
+                self.rect.x, self.rect.y - self.STEP)
+
+    def lending(self):
+        self.rect = self.rect.move(
+            self.rect.x, self.rect.y + self.STEP)
+
+        if pygame.sprite.spritecollideany(self, tiles_group):
+            self.rect = self.rect.move(
+                self.rect.x, self.rect.y - self.STEP)
+            self.is_falling = False
 
 
 class Background(pygame.sprite.Sprite):
@@ -137,37 +157,45 @@ def generate_level(level):
 
             elif level[y][x] == '@':
                 new_player = Player(x, y)
+
     return new_player, x, y
 
 
 background = Background()
 
-level = ['..............',
-         '##..##..##.###',
+level = ['      ',
+         '     /?',
+         '     ',
          '@ ##  ##  #',
-         '      ',
-         '       ',
-         '    /?']
+         '##..##..##.###',
+         '...............',]
+
 new_player, x, y = generate_level(level)
 
 while not_exit:
+    pygame.display.set_caption('platformer')
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             not_exit = False
+
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
                 new_player.is_move = True
                 new_player.direction = 'left'
+
             if event.key == pygame.K_d:
                 new_player.is_move = True
                 new_player.direction = 'right'
+
             if event.key == pygame.K_SPACE:
-                print('fwneid')
+                print('драсте забор покрасьте')
                 new_player.up_time = pygame.time.get_ticks()
                 new_player.is_up = True
-                new_player.direction = 'right'  # up
+                new_player.direction = 'right'
+
         elif event.type == pygame.KEYUP:
             new_player.is_move = False
+
     new_player.update()
     all_sprites.draw(screen)
     pygame.display.flip()
